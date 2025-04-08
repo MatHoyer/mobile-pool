7e3d7b46e8ba030c56e8f0469d3ea34e5580bf9c
MatHoyer <mathieuhoyer@gmail.com>
2025-04-08 13:20:54 +0200

    forgot for all pages

--- mobileModule01/app/ex01/(tabs)/today.tsx
+++ mobileModule01/app/ex01/(tabs)/today.tsx
@@ -1,10 +1,14 @@
 import { styles } from '@/assets/styles';
+import useLocationStore from '@/hooks/locationStore';
 import { SafeAreaView, Text } from 'react-native';
 
 const TodayTab = () => {
+  const location = useLocationStore((state) => state.location);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.largeText}>Today</Text>
+      <Text>{location}</Text>
     </SafeAreaView>
   );
 };
--- mobileModule01/app/ex01/(tabs)/weekly.tsx
+++ mobileModule01/app/ex01/(tabs)/weekly.tsx
@@ -1,10 +1,14 @@
 import { styles } from '@/assets/styles';
+import useLocationStore from '@/hooks/locationStore';
 import { SafeAreaView, Text } from 'react-native';
 
 const WeeklyTab = () => {
+  const location = useLocationStore((state) => state.location);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.largeText}>Weekly</Text>
+      <Text>{location}</Text>
     </SafeAreaView>
   );
 };
