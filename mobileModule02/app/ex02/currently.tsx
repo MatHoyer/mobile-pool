df96729906b1085d9ee88db7fd4298e2ca80354f
MatHoyer <mathieuhoyer@gmail.com>
2025-12-03 17:52:52 +0100

    fix: module02 exo02

---
+++ mobileModule02/app/ex02/currently.tsx
@@ -0,0 +1,81 @@
+import { styles } from "@/assets/styles";
+import Typography from "@/components/Typography";
+import useLocationStore from "@/hooks/locationStore";
+import { weatherCodeToCondition } from "@/lib/utils";
+import { Wind } from "lucide-react-native";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+type TCurrentWeather = {
+  temperature: number;
+  windSpeed: number;
+  weatherCode: number;
+};
+
+const CurrentlyTab = () => {
+  const location = useLocationStore((state) => state.location);
+  const [currentWeather, setCurrentWeather] = useState<TCurrentWeather | null>(null);
+
+  useEffect(() => {
+    if (!location) return;
+
+    const fetchCurrentWeather = async () => {
+      try {
+        const weather = await fetch(
+          `https://api.open-meteo.com/v1/forecast?latitude=${location.lat}&longitude=${location.lon}&current=temperature_2m,wind_speed_10m,weathercode&timezone=Europe%2FParis`
+        );
+        if (!weather.ok) {
+          throw new Error("Failed to fetch weather data");
+        }
+        const currentwether = await weather.json();
+        const parsedData = {
+          temperature: currentwether.current.temperature_2m,
+          windSpeed: currentwether.current.wind_speed_10m,
+          weatherCode: currentwether.current.weathercode,
+        };
+        setCurrentWeather(parsedData);
+      } catch {
+        setCurrentWeather(null);
+      }
+    };
+
+    fetchCurrentWeather();
+  }, [location]);
+
+  if (!location) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Typography variant="large" style={{ color: "red", textAlign: "center" }}>
+          Geolocation is not available, please enable it in your settings.
+        </Typography>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{ flexDirection: "column", alignItems: "center", gap: 10 }}>
+        {currentWeather?.temperature !== undefined && (
+          <Typography variant="h1">{currentWeather?.temperature}°</Typography>
+        )}
+        {currentWeather?.weatherCode !== undefined && (
+          <Typography>{weatherCodeToCondition(currentWeather?.weatherCode)}</Typography>
+        )}
+        <Typography variant="large">{location.name}</Typography>
+        <View style={{ flexDirection: "row", gap: 10 }}>
+          <Typography>{location.region}</Typography>
+          <Typography>{location.country}</Typography>
+        </View>
+        {currentWeather?.windSpeed !== undefined && (
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+            <Wind />
+            <Typography>{currentWeather?.windSpeed} km/h</Typography>
+          </View>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default CurrentlyTab;
