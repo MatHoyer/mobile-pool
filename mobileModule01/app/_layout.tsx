2314c1bd1a0053dbcf342a2486f8c8c2900b4cd9
MatHoyer <mathieuhoyer@gmail.com>
2025-12-06 21:29:12 +0100

    fix: only one app

--- mobileModule01/app/_layout.tsx
+++ mobileModule01/app/_layout.tsx
@@ -1,11 +1,13 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
 
-export default function RootLayout() {
+const TabLayout = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <Stack screenOptions={{ headerShown: false }} />
     </>
   );
-}
+};
+
+export default TabLayout;
