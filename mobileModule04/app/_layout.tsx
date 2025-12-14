73122b9767dcfa5a2278b9f92897aa4669080d28
MatHoyer <mathieuhoyer@gmail.com>
2025-12-14 17:00:29 +0100

    setup firebase

--- mobileModule04/app/_layout.tsx
+++ mobileModule04/app/_layout.tsx
@@ -1,5 +1,6 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "../firebaseConfig";
 
 const RootLayout = () => {
   return (
--- mobileModule04/app/index.tsx
+++ mobileModule04/app/index.tsx
@@ -1,15 +1,34 @@
+import { styles } from "@/assets/styles";
+import Button from "@/components/Button";
+import Typography from "@/components/Typography";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-/**
- * display: "flex",
- * flexDirection: "column-reverse"
- *
- * This is to ensure that the tab bar is always at the bottom of the screen, regardless of the content of the screen.
- * And we need to put the weather app bar above the tab bar. because react native tab render catch the click otherwise.
- */
-
 const TabViewIndex = () => {
-  return <SafeAreaView style={{ flex: 1, display: "flex", flexDirection: "column-reverse" }}></SafeAreaView>;
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(getAuth(), setUser);
+    return subscriber;
+  }, []);
+
+  if (!user) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Button
+          onPress={() => console.log("Login")}
+          style={{
+            width: "auto",
+          }}
+        >
+          <Typography style={styles.buttonText}>Login</Typography>
+        </Button>
+      </SafeAreaView>
+    );
+  }
+
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
 };
 
 export default TabViewIndex;
